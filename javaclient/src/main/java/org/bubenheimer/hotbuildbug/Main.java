b39cf3cd02b1ba0e9b6c7c9f333eee7095186c33
Uli Bubenheimer <bubenheimer@users.noreply.github.com>
2017-11-14 17:30:45 -0600

    Initial commit

---
+++ javaclient/src/main/java/org/bubenheimer/hotbuildbug/Main.java
@@ -0,0 +1,7 @@
+package org.bubenheimer.hotbuildbug;
+
+public class Main {
+    public static void main(final String[] args) throws InterruptedException {
+        Thread.sleep(10_000_000L);
+    }
+}
